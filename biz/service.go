99c7b678258e6ccaca9870f2826bd0320ac69569
Ryan Fan <reg_info@qq.com>
2025-09-13 14:03:18 +0800

    add service context (#38)
    Co-authored-by: hdget <hdget@hdget.com>

--- biz/service.go
+++ biz/service.go
@@ -3,13 +3,17 @@ package biz
 import (
 	"context"
 
-	"github.com/hdget/sdk/dapr/meta"
+	"github.com/hdget/common/constant"
+	"github.com/hdget/common/meta"
+	"github.com/hdget/common/servicectx"
 )
 
 type Service interface {
+	Context() context.Context
 	GetTid() int64
 	GetUid() int64
-	Context() context.Context
+	GetAppId() string
+	GetTsn() string
 }
 
 type bizSvcImpl struct {
@@ -17,7 +21,7 @@ type bizSvcImpl struct {
 }
 
 func NewService(ctx context.Context) Service {
-	return &bizSvcImpl{ctx: ctx}
+	return &bizSvcImpl{ctx: servicectx.New(ctx)}
 }
 
 func (s bizSvcImpl) Context() context.Context {
@@ -25,9 +29,17 @@ func (s bizSvcImpl) Context() context.Context {
 }
 
 func (s bizSvcImpl) GetTid() int64 {
-	return meta.New().GetTid(s.ctx)
+	return meta.FromServiceContext(s.ctx).GetInt64(constant.MetaKeyTid)
 }
 
 func (s bizSvcImpl) GetUid() int64 {
-	return meta.New().GetUid(s.ctx)
+	return meta.FromServiceContext(s.ctx).GetInt64(constant.MetaKeyTid)
+}
+
+func (s bizSvcImpl) GetAppId() string {
+	return meta.FromServiceContext(s.ctx).GetString(constant.MetaKeyAppId)
+}
+
+func (s bizSvcImpl) GetTsn() string {
+	return meta.FromServiceContext(s.ctx).GetString(constant.MetaKeyTsn)
 }
