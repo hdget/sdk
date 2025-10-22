2bb18aa3f6ffb79107b69f128376a8d9d38b6e25
Ryan Fan <reg_info@qq.com>
2025-10-22 10:44:01 +0800

    Feat/normalize (#46)
    * separate namespace and app with dash
    
    ---------
    
    Co-authored-by: hdget <hdget@hdget.com>

--- dapr/utils/utils.go
+++ dapr/utils/utils.go
@@ -7,9 +7,19 @@ import (
 	"strings"
 )
 
+const (
+	// 命名空间和真实名字的分割符号
+	namespaceNameSeparator = "-"
+)
+
 func Normalize(input string) string {
 	if namespace, exists := os.LookupEnv(constant.EnvKeyNamespace); exists {
-		return namespace + "_" + input
+		var sb strings.Builder
+		sb.Grow(len(namespace) + len(input) + 1)
+		sb.WriteString(namespace)
+		sb.WriteString(namespaceNameSeparator)
+		sb.WriteString(input)
+		return sb.String()
 	}
 	return input
 }
