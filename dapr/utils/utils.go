78b37f3c0b63c4059efc6d6cfe55daeff389881b
hdget <hdget@hdget.com>
2025-10-22 10:42:08 +0800

    separate namespace and app with dash

--- dapr/utils/utils.go
+++ dapr/utils/utils.go
@@ -2,14 +2,25 @@ package utils
 
 import (
 	"fmt"
-	"github.com/hdget/common/constant"
 	"os"
 	"strings"
+
+	"github.com/hdget/common/constant"
+)
+
+const (
+	// 命名空间和真实名字的分割符号
+	namespaceNameSeparator = "-"
 )
 
 func Normalize(input string) string {
 	if namespace, exists := os.LookupEnv(constant.EnvKeyNamespace); exists {
-		return namespace + "_" + input
+		var sb strings.Builder
+		sb.Grow(len(namespace) + len(input) + 1)
+		sb.WriteString(namespace)
+		sb.WriteString(namespaceNameSeparator)
+		sb.WriteString(input)
+		return sb.String()
 	}
 	return input
 }
