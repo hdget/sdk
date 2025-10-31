f7782232724b5c30ddd460eb264ed3091e55e8da
Ryan Fan <reg_info@qq.com>
2025-10-31 16:07:49 +0800

    change generate method (#51)

--- dapr/utils/utils.go
+++ dapr/utils/utils.go
@@ -5,29 +5,31 @@ import (
 	"strings"
 )
 
-func GenerateMethod(version int, module, handler string, apiEndpoint ...string) string {
+func GenerateMethod(version int, module, handler string, endpoint ...string) string {
 	var builder strings.Builder
 
 	// 预分配足够的内存以避免扩容开销
-	// 估算基础长度：":v" (2) + 版本号（假设1-2位） + module长度 + handler长度 + 可能的AppEndpoint长度 + 分隔符":"
-	estimatedLength := 2 + len(module) + len(handler) + len(":")
-	for _, c := range apiEndpoint {
-		estimatedLength += len(c)
+	// e,g:
+	// http调用：v1:endpoint:module:handler
+	// 内部直接调用：v1:module:handler
+	// 估算基础长度：v (1) + 版本号（假设1位）+ 2分隔符":" + module长度 + handler长度
+	estimatedLength := 4 + len(module) + len(handler)
+	if len(endpoint) > 0 {
+		estimatedLength += len(endpoint[0]) + 1
 	}
 	builder.Grow(estimatedLength)
 
 	// 直接写入，避免任何中间的字符串拼接
 	builder.WriteString("v")
 	builder.WriteString(strconv.Itoa(version))
+	if len(endpoint) > 0 {
+		builder.WriteString(":")
+		builder.WriteString(endpoint[0])
+	}
 	builder.WriteString(":")
 	builder.WriteString(module)
 	builder.WriteString(":")
 	builder.WriteString(handler)
-
-	if len(apiEndpoint) > 0 && apiEndpoint[0] != "" {
-		builder.WriteString(":")
-		builder.WriteString(apiEndpoint[0])
-	}
 
 	return strings.ToLower(builder.String())
 }
