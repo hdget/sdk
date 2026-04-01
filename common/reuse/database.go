c760f8535353ee994aca5152a02a4db69e4a0d38
rfancn <reg_info@qq.com>
2026-04-01 16:25:57 +0800

    add reuse

---
+++ common/reuse/database.go
@@ -0,0 +1,87 @@
+package reuse
+
+import (
+	"github.com/hdget/sdk/common/protobuf"
+)
+
+// Database
+
+type DbOperation[TObjectKey ObjectKeyType, TBizObject any, TModelObject any, TCondition any] interface {
+	DbCreate[TBizObject, TModelObject]
+	DbRetrieve[TObjectKey, TModelObject, TCondition]
+	DbUpdate[TModelObject]
+	DbDelete[TObjectKey]
+}
+
+// DbCreate 创建操作:C
+type DbCreate[BizObject any, ModelObject any] interface {
+	Create(bizObj BizObject) (ModelObject, error) // 创建对象
+}
+
+// DbRetrieve 读操作
+type DbRetrieve[TObjectKey ObjectKeyType, TModelObject any, TCondition any] interface {
+	Get(key TObjectKey) (TModelObject, error)                                            // 获取对象
+	Count(filters map[string]string) (int64, error)                                      // 统计对象
+	List(filters map[string]string, list ...*protobuf.ListParam) ([]TModelObject, error) // 列出对象, list不传的时候获取所有对象
+	GetQueryConditions(filters map[string]string) []TCondition                           // 获取查询条件
+}
+
+// DbUpdate 更新：U
+type DbUpdate[TModelObject any] interface {
+	Update(modelObj TModelObject) error // 更新某个字段
+}
+
+type DbEdit[TBizObject any] interface {
+	Edit(bizObj TBizObject) error // 编辑对象
+}
+
+// DbDelete 删除
+type DbDelete[TObjectKey ObjectKeyType] interface {
+	Delete(key TObjectKey) error // 删除对象
+}
+
+// DbBulkRetrieve 批量读取
+type DbBulkRetrieve[TObjectKey ObjectKeyType, ModelObject any] interface {
+	BulkGet(ids []TObjectKey) (map[TObjectKey]ModelObject, error) // 批量获取对象
+}
+
+/* 关联数据表 */
+
+type RefDbOperation[TObjectKey ObjectKeyType, TRefBizObject any, TRefModelObject any, TCondition any] interface {
+	RefDbCreate[TObjectKey, TRefBizObject, TRefModelObject]
+	RefDbRetrieve[TObjectKey, TRefModelObject, TCondition]
+	RefDbUpdate[TRefModelObject]
+	RefDbDelete[TObjectKey]
+}
+
+// RefDbCreate 创建关联对象操作:C
+type RefDbCreate[TObjectKey ObjectKeyType, TRefBizObject any, TRefModelObject any] interface {
+	Create(key TObjectKey, refBizObj TRefBizObject) (TRefModelObject, error) // 创建关联对象DAO
+}
+
+// RefDbRetrieve 读取关联对象操作:R
+type RefDbRetrieve[TObjectKey ObjectKeyType, TRefModelObject any, Condition any] interface {
+	Get(key, refKey TObjectKey) (TRefModelObject, error)                                                          // 获取关联对象DAO
+	Count(key TObjectKey, refObjFilters map[string]string) (int64, error)                                         // 统计关联对象DAO
+	List(key TObjectKey, refObjFilters map[string]string, list ...*protobuf.ListParam) ([]TRefModelObject, error) // 列出关联对象DAO
+	GetQueryConditions(key TObjectKey, refObjFilters map[string]string) []Condition                               // 获取关联对象DAO
+}
+
+// RefDbUpdate 更新关联对象：U
+type RefDbUpdate[TRefObjectModel any] interface {
+	Update(refObjModel TRefObjectModel) error // 更新数据库关联对象
+}
+
+type RefDbEdit[TObjectKey ObjectKeyType, TRefBizObject any] interface {
+	Edit(key TObjectKey, refBizObj TRefBizObject) error // 编辑数据库关联对象DAO
+}
+
+// RefDbDelete 删除关联对象
+type RefDbDelete[TObjectKey ObjectKeyType] interface {
+	Delete(key, refKey TObjectKey) error // 删除关联对象DAO
+}
+
+// RefDbBulkRetrieve 批量读取关联对象
+type RefDbBulkRetrieve[TObjectKey ObjectKeyType, TRefModelObject any] interface {
+	BulkGet(key TObjectKey, refKeys []TObjectKey) (map[TObjectKey]TRefModelObject, error) // 批量获取对象
+}
---
+++ common/reuse/object.go
@@ -0,0 +1,33 @@
+package reuse
+
+import "github.com/hdget/sdk/common/protobuf"
+
+type ObjectKeyType interface {
+	int64 | int32 | int | string
+}
+
+/* request */
+
+type OperateObjectRequest[TObjectKey ObjectKeyType] struct {
+	Key TObjectKey `json:"key"`
+}
+
+type BulkOperateObjectRequest[TObjectKey ObjectKeyType] struct {
+	Keys []TObjectKey `json:"keys"`
+}
+
+type QueryObjectRequest struct {
+	Filters map[string]string   `json:"filters,omitempty"`
+	List    *protobuf.ListParam `json:"list,omitempty"`
+}
+
+/* response */
+
+type CreateObjectResponse[TObjectId ObjectKeyType] struct {
+	Id TObjectId `json:"id"`
+}
+
+type QueryObjectResponse[TBizObject any] struct {
+	Total int64        `json:"total"`
+	Items []TBizObject `json:"items"`
+}
