8a6725ef46e44cc9831cdb4546f693b75762904f
Ryan Fan <reg_info@qq.com>
2026-05-07 12:09:30 +0800

    add GetClientIP (#112)

--- common/bizctx/meta_key.go
+++ common/bizctx/meta_key.go
@@ -1,11 +1,12 @@
 package bizctx
 
 const (
-	MetaKeyAppKey  = "hd-app-id"   // 应用ID
-	MetaKeySource  = "hd-source"   // 请求来源, 例如：第三方API回调
-	MetaKeyAppCode = "hd-app-code" // 应用类型Code
-	MetaKeyTid     = "hd-tid"      // tenant id
-	MetaKeyUid     = "hd-uid"      // user id
-	MetaKeyUsn     = "hd-usn"      // user sn
-	MetaKeyRoleIds = "hd-role-ids" // role ids
+	MetaKeyAppKey   = "hd-app-id"    // 应用ID
+	MetaKeySource   = "hd-source"    // 请求来源, 例如：第三方API回调
+	MetaKeyAppCode  = "hd-app-code"  // 应用类型Code
+	MetaKeyTid      = "hd-tid"       // tenant id
+	MetaKeyUid      = "hd-uid"       // user id
+	MetaKeyUsn      = "hd-usn"       // user sn
+	MetaKeyRoleIds  = "hd-role-ids"  // role ids
+	MetaKeyClientIP = "hd-client-ip" // client ip
 )
